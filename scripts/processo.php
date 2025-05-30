8727afaeca1ad4df6a4ce96ca7b4c94768a856fc
henrique1oo2 <henriquesimoes259@gmail.com>
2025-05-30 13:08:30 +0100

    update test drive

--- scripts/processo.php
+++ scripts/processo.php
@@ -1,9 +1,9 @@
 <?php
 // Configurações da base de dados
-$servername = "localhost"; // Geralmente 'localhost'
-$username = "root";       // O seu nome de utilizador da base de dados (Ex: 'root' para XAMPP/WAMP)
-$password = "";           // A sua palavra-passe da base de dados (Ex: vazio para XAMPP/WAMP)
-$dbname = "deapc";        // O nome da sua base de dados
+$servername = "localhost"; 
+$username = "root";       
+$password = "";           
+$dbname = "deapc";        
 
 // Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,12 +13,11 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Verifica se a requisição é do tipo POST (ou seja, se o formulário foi submetido)
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Obtém e sanitiza os dados do formulário
-    $test_drive_date = $_POST['test_drive_date']; // A data selecionada
-    $car_model = $_POST['car_model'];             // O nome do modelo do carro (SIT Pizza, SIT Leão, etc.)
-    $process_type = $_POST['process_type'];       // O tipo de processo, que será "Test Drive"
-
+    $test_drive_date = $_POST['test_drive_date']; 
+    $car_model = $_POST['car_model'];             
+    $process_type = $_POST['process_type'];       
     // Prepara a query SQL para inserir os dados
-    // O 'id' é AUTO_INCREMENT, então a base de dados irá gerá-lo automaticamente.
+    
     $sql = "INSERT INTO processo (tipo, data_test_drive) VALUES (?, ?)";
 
     // Prepara a declaração para prevenir SQL injection
@@ -31,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Executa a declaração
     if ($stmt->execute()) {
-        $last_id = $stmt->insert_id; // Obtém o ID que foi gerado automaticamente
+        $last_id = $stmt->insert_id; 
         echo "Test Drive para o modelo '" . htmlspecialchars($car_model) . "' agendado com sucesso para " . htmlspecialchars($test_drive_date) . ". ID do Processo: " . $last_id;
         echo "<br><br><a href='../catalogo.html'>Voltar ao Catálogo</a>"; // Adiciona um link para voltar
     } else {
