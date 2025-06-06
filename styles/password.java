02269a7139f14e4c4bc7ef4f1926219b424485df
henrique1oo2 <henriquesimoes259@gmail.com>
2025-06-06 12:03:02 +0100

    Update password.java

---
+++ styles/password.java
@@ -0,0 +1,7 @@
+javac : The term 'javac' is not recognized as the name of a cmdlet, function, script file, or operable program. Check the spelling of the name, or if a path 
+was included, verify that the path is correct and try again.
+At line:1 char:1
++ javac password.java
++ ~~~~~
+    + CategoryInfo          : ObjectNotFound: (javac:String) [], CommandNotFoundException
+    + FullyQualifiedErrorId : CommandNotFoundException
